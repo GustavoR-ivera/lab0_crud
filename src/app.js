f9515119926fa7845caebe36fc455758b759b9dd
matiuss <47614578+matiuss@users.noreply.github.com>
2024-04-18 00:14:51 -0500

    Update app.js(mysql2)

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const mysql = require('mysql');
-const mysql = require("mysql2");
+const mysql2 = require("mysql2");
 const myConnection = require("express-myconnection");
 
 // import routes
